83f64c124c34219e943d700fb36d8dd03fef9383
Keith <suenkaleongkeith@gmail.com>
2019-01-15 00:23:32 +0800

    update check herbs

--- ChineseHerb db generator/checkHerbsInDB.py
+++ ChineseHerb db generator/checkHerbsInDB.py
@@ -4,14 +4,27 @@ herbs = []
 targets = []
 # herb_id	engName	chiName	effect	taste	quality	shape
 with open('herbs.csv', newline='') as csvfile:
-	herbs = csv.DictReader(csvfile)
+	rows = csv.DictReader(csvfile)
+	for row in rows:
+		herbs.append(row)
 
 with open('herbs_unique.txt', newline='') as csvfile:
-	targets = csv.reader(csvfile)
+	rows = csv.reader(csvfile)
+	for row in rows:
+		targets.append(row)
 
 with open('checked_herbs_unique.txt', 'w', newline='') as csvfile:
 	writer = csv.writer(csvfile)
+	writer.writerow(['herb_id','engName','chiName','effect','taste','quality','shape'])
 	s = set(targets)
+	i = 1
 	for herb in herbs:
 		if herb['chiName'] in s:
-			writer.writerow(herb)
+			if(i<10):
+				herb_id = "000" + str(i)
+			elif(i<100):
+				herb_id = "00" + str(i)
+			else:
+				herb_id = "0" + str(i)
+			writer.writerow([herb_id,herb['engName'],herb['chiName'],herb['effect'],herb['taste'],herb['quality'],herb['shape']])
+			i+=1
