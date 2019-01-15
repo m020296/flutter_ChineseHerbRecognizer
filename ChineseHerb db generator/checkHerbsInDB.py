41aab11b2396a6619818239ec1c8659908c89aa5
Keith <suenkaleongkeith@gmail.com>
2019-01-15 14:50:10 +0800

    checked herbs db

--- ChineseHerb db generator/checkHerbsInDB.py
+++ ChineseHerb db generator/checkHerbsInDB.py
@@ -11,20 +11,24 @@ with open('herbs.csv', newline='') as csvfile:
 with open('herbs_unique.txt', newline='') as csvfile:
 	rows = csv.reader(csvfile)
 	for row in rows:
-		targets.append(row)
+		for col in row:
+			targets.append(col)
 
-with open('checked_herbs_unique.txt', 'w', newline='') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow(['herb_id','engName','chiName','effect','taste','quality','shape'])
-	s = set(targets)
-	i = 1
-	for herb in herbs:
-		if herb['chiName'] in s:
-			if(i<10):
-				herb_id = "000" + str(i)
-			elif(i<100):
-				herb_id = "00" + str(i)
+with open('missed_herbs.csv', 'w', newline='') as missedfile:
+	with open('checked_herbs_db.csv', 'w', newline='') as csvfile:
+		missed = csv.writer(missedfile)
+		writer = csv.writer(csvfile)
+		writer.writerow(['herb_id','engName','chiName','effect','taste','quality','shape'])
+		i = 1
+		for herb in herbs:
+			if herb['chiName'] in targets:
+				if(i<10):
+					herb_id = "000" + str(i)
+				elif(i<100):
+					herb_id = "00" + str(i)
+				else:
+					herb_id = "0" + str(i)
+				writer.writerow([herb_id,herb['engName'],herb['chiName'],herb['effect'],herb['taste'],herb['quality'],herb['shape']])
+				i+=1
 			else:
-				herb_id = "0" + str(i)
-			writer.writerow([herb_id,herb['engName'],herb['chiName'],herb['effect'],herb['taste'],herb['quality'],herb['shape']])
-			i+=1
+				missed.writerow([herb['chiName']])
