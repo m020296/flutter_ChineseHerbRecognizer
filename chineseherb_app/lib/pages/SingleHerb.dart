78111161f759b3e4d49f8167f45fb346e88d36bb
Shum Hiu Tung <mike.sam.hell@gmail.com>
2019-03-11 01:00:52 +0800

    Single herb can query

--- chineseherb_app/lib/pages/SingleHerb.dart
+++ chineseherb_app/lib/pages/SingleHerb.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:photo_view/photo_view.dart';
-import '../customeWidgets/classifyFloatBtn.dart';
+import 'package:tflite/tflite.dart';
 
 class SingleHerb extends StatefulWidget {
   @override
@@ -11,16 +13,17 @@ class SingleHerb extends StatefulWidget {
 
 class _SingleHerbState extends State<SingleHerb> {
   @override
+  Uint8List finalImageBytes;
+  String currentHerb = "Please take photo";
   Widget build(BuildContext context) {
-    Uint8List finalImageBytes;
     if (finalImageBytes == null) {
       return new Scaffold(
-          backgroundColor: Colors.grey,
+          backgroundColor: Colors.grey[300],
           // appBar: new AppBar(
           //   title: new Text("多種中藥辨識"),
           //   backgroundColor: Colors.green[900],
           // ),
-          body: new Center(child: new Text("MultiHerb2")),
+          body: new Center(child: new Text("Single Herb")),
           floatingActionButton: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.end,
@@ -30,7 +33,9 @@ class _SingleHerbState extends State<SingleHerb> {
                   Icons.camera,
                   color: Colors.white,
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  _detectImagebyCamera(context);
+                },
               ),
               Padding(
                 padding: const EdgeInsets.all(5.0),
@@ -71,7 +76,9 @@ class _SingleHerbState extends State<SingleHerb> {
                 Icons.camera,
                 color: Colors.white,
               ),
-              onPressed: () {},
+              onPressed: () {
+                _detectImagebyCamera(context);
+              },
             ),
             FloatingActionButton(
               child: Icon(
@@ -86,5 +93,30 @@ class _SingleHerbState extends State<SingleHerb> {
             ),
           ],
         ));
+  }
+
+  _detectImagebyCamera(BuildContext context) async {
+    File image;
+    image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    List<int> imageBytes = image.readAsBytesSync();
+
+    String res = await Tflite.loadModel(
+      model: "assets/output_graph.tflite",
+      labels: "assets/output_labels.txt",
+    );
+    var recognitions = await Tflite.runModelOnImage(
+        path: image.path, // required
+        imageMean: 0.0, // defaults to 117.0
+        imageStd: 255.0, // defaults to 1.0
+        numResults: 5, // defaults to 5
+        threshold: 0.1 // defaults to 0.1
+        );
+    print(recognitions.toString());
+
+    setState(() {
+      finalImageBytes = imageBytes;
+      return finalImageBytes;
+    });
   }
 }
